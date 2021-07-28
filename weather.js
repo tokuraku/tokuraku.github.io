afb0b87ab14304267eeb62d8f2448ecfd293dbb8
tokuraku <88010068+tokuraku@users.noreply.github.com>
2021-07-28 18:21:36 +0900

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+	if (location.hash == '#weather') {
+		var url = '/weather.'
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				const key = '123456';
+				const md5 = CryptoJS.MD5(key).toString();
+				const iv = md5.substr(0, 16);
+				const data = xhr.responseText;
+				const uKey = parseKey(md5);
+				const uIv = parseKey(iv);
+				
+				let bytes = CryptoJS.AES.decrypt(text.value, uKey,
+				{
+					iv: uIv,
+					mode: CryptoJS.mode.CBC,
+					padding: CryptoJS.pad.Pkcs7
+				});
+				
+				var resHTML = document.createElement('html');
+				resHTML.innerHTML = bytes.toString(CryptoJS.enc.Utf8);
+			}
+		}
+		xhr.send();
+	}
+}
