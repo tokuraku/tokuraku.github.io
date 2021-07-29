625965dcc73a85aa1544a61d40d05e12622870f6
tokuraku <88010068+tokuraku@users.noreply.github.com>
2021-07-30 06:42:37 +0900

    Update rader.js

--- rader.js
+++ rader.js
@@ -1,9 +1,9 @@
-window.onload = function(e){
+/*window.onload = function(e){
   var img = document.querySelector('img')
   var pres = document.querySelectorAll('pres');
   
   
-}
+}*/
 function testAlert() {
   alert('Hey!')
 }
