a11ea591cf6b8b72a6a66ff609e3fcb515355f67
tokuraku <88010068+tokuraku@users.noreply.github.com>
2021-07-30 12:00:25 +0900

    Create various.js

---
+++ various.js
@@ -0,0 +1,16 @@
+function parseKey(key) {
+	return CryptoJS.enc.Utf8.parse(key);
+}
+function decrypt(mode, cipherText, key) {
+	const md5 = CryptoJS.MD5(key).toString();
+	const iv = md5.substr(0, 16);
+	const uKey = parseKey(md5);
+	const uIv = parseKey(iv);
+
+	let bytes = CryptoJS.AES.decrypt(cipherText, uKey, {
+		iv: uIv,
+		mode: mode,
+		padding: CryptoJS.pad.Pkcs7
+	});
+	return bytes.toString(CryptoJS.enc.Utf8);
+}
