279222122d7df21e80eb1c4ec3416cbd96abe1eb
tokuraku <88010068+tokuraku@users.noreply.github.com>
2021-08-30 06:55:16 +0900

    Update static.js

--- static.js
+++ static.js
@@ -9,9 +9,10 @@ var text3 = `3週間(21日)後は${d3.getMonth()+1}/${d3.getDate()}です`;
         
 var d4 = new Date();
 var d5 = new Date();
-d4.setFullYear(d4.getFullYear()-15);
+d4.setFullYear(d4.getFullYear()-16);
+d4.setDate(d4.getDate()+1);
 d5.setFullYear(d5.getFullYear()-12);
-var l = [d4.getFullYear(),d4.getMonth()+1,d4.getDate(),d5.getFullYear(),d5.getMonth()+1,d5.getDate()]
+var l = [d4.getFullYear(),d4.getMonth()+1,d4.getDate(),d5.getFullYear(),d5.getMonth()+1,d5.getDate()];
 var textC = `12歳から15歳の範囲は ${l[0]}/${l[1]}/${l[2]} から ${l[3]}/${l[4]}/${l[5]} です`;
 
 var par = document.querySelector('div#static');
